30ba5e6cf7d9bd08846d00ffdb8b5b843b3a6f53
Mustard <mustard.mar@gmail.com>
2026-02-03 19:33:45 +0300

    Пользователь голосует за определенный пункт 1 раз, при достижении опр. числа голосов пункт помечается(*), добавлена кнопка(inline) "обновить бинго" и кнопки "принять участие" и "получить бинго"
    На будущее добавлен код для бд календарь и бд кнопок-пунктов, чтобы отслеживать состояние бинго в бд(только объявление). Удалён лишний код.

--- Example and tests/telebot_db.py
+++ Example and tests/telebot_db.py
@@ -1,15 +1,31 @@
 import sqlite3
 
-def create_table():
+def create_table_items():
     connection = sqlite3.connect('my_database.db')
     cursor = connection.cursor()
+    # таблица для пунктов бинго
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Items (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name_item TEXT NOT NULL
-    )
-    ''')
-
+        CREATE TABLE IF NOT EXISTS Items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name_item TEXT NOT NULL
+        )
+        ''')
+    #таблица для кнопок-пунктов
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS MenuBingo (
+        button_id INTEGER PRIMARY KEY,
+        count INTEGER DEFAULT 0
+        )
+        ''')
+    #таблица для голосов(пользователей)
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Votes (
+        button_id INTEGER,
+        user_id INTEGER,
+        PRIMARY KEY (button_id,user_id),
+        FOREIGN KEY (item_id) REFERENCES Items (item_id)
+        )
+        ''')
     connection.commit()
     connection.close()
 def add_new_item(msg):
@@ -39,5 +55,49 @@ def db_delete_item(value):
 
     connection.commit()
     connection.close()
-#add_new_item("Test 2")
-#print(get_all_items())
+
+
+def create_table_calendar():
+    connection = sqlite3.connect('calendar.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    date TEXT NOT NULL
+    )
+    ''')
+    connection.commit()
+    connection.close()
+def add_new_event(title, date, description = ""):
+    connection = sqlite3.connect('calendar.db')
+    cursor = connection.cursor()
+    if description=="":
+        cursor.execute('INSERT INTO Events (title,date) VALUES (?,?)', (title,date))
+    else:
+        cursor.execute('INSERT INTO Events (title,date,description) VALUES (?,?,?)', (title, date,description))
+    connection.commit()
+    connection.close()
+def get_all_events():
+    connection = sqlite3.connect('calendar.db')
+    cursor = connection.cursor()
+
+    # Выбираем всех пользователей
+    cursor.execute('SELECT * FROM Events')
+    items = cursor.fetchall()
+
+    connection.close()
+
+    return items
+def delete_db_calendar():
+    connection = sqlite3.connect('calendar.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+        DELETE FROM Events
+        ''')
+    connection.commit()
+    connection.close()
+
+#create_table_calendar()
+#print(get_all_events())
--- Example and tests/test_telebot.py
+++ Example and tests/test_telebot.py
@@ -9,14 +9,27 @@ load_dotenv()
 token = os.getenv("BOT_TOKEN")
 bot=telebot.TeleBot(token)
 
-counters = {f'item{i}': 0 for i in range(1, 13)}
+#Заглушка для бд
+counters={}
+for i in range(1, 13):
+    counters[f'item{i}']=[0,[]]
+
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(
-        chat_id=message.chat.id,
-        text='Привет!'
-    )
+    markup = types.ReplyKeyboardMarkup()
+    btn_get_bingo = types.KeyboardButton('Получить бинго')
+    btn_join_bingo = types.KeyboardButton('Принять участие в бинго')
+    markup.add(btn_get_bingo)
+    markup.add(btn_join_bingo)
+    bot.send_message(message.chat.id, "Участвуйте в бинго!", reply_markup=markup)
 
+@bot.message_handler(func=lambda message: message.text in ["Получить бинго","Принять участие в бинго"])
+def handle_button(message):
+    if message.text == "Получить бинго":
+        welcome(message)
+    if message.text == "Принять участие в бинго":
+        print(f"Пользователь {message.from_user.id} принял участие в бинго")
+#for admin
 @bot.message_handler(commands=['button'])
 def send_keyboard(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -24,7 +37,7 @@ def send_keyboard(message):
     keyboard.add(types.KeyboardButton("Узнать все пункты"))
     keyboard.add(types.KeyboardButton("Удалить пункт"))
     bot.send_message(message.chat.id, "Выберите кнопку:", reply_markup=keyboard)
-
+#for admin
 @bot.message_handler(func=lambda message: message.text in ["Добавить пункт","Узнать все пункты", "Удалить пункт"])
 def handle_button(message):
     if message.text == "Добавить пункт":
@@ -37,11 +50,11 @@ def handle_button(message):
         items = telebot_db.get_all_items()
         text_items = "\n".join([f"{i}: {text}" for i, text in items])
         bot.send_message(message.chat.id, text_items)
-
+#for admin
 def add_item(message):
     telebot_db.add_new_item(message.text)
     bot.send_message(message.chat.id, 'Добавлено!')
-
+#for admin
 def delete_item(message):
 
     try:
@@ -50,65 +63,68 @@ def delete_item(message):
         bot.send_message(message.chat.id, 'Удалено!')
     except ValueError:
         bot.send_message(message.chat.id, 'Ошибка!')
-
+#bingo_button
 def create_keyboard():
     markup = types.InlineKeyboardMarkup()
+    maxamount = 3
+    text_complete = "✅"
+    buttons = []
+    for i in range(1, 13):
+        key = f'item{i}'
+        if counters[key][0]!=maxamount:
+            text = f"{i} ({counters[key][0]}/{maxamount})"
+        else:
+            text = f"{i} {text_complete}"
+        buttons.append(telebot.types.InlineKeyboardButton(text=text, callback_data=key))
+    markup.add(*buttons, row_width=4)
 
-    button_1 = telebot.types.InlineKeyboardButton(text=f"1 ({counters['item1']}/{15})", callback_data='item1')
-    button_2 = telebot.types.InlineKeyboardButton(text=f"2 ({counters['item2']}/{15})", callback_data='item2')
-    button_3 = telebot.types.InlineKeyboardButton(text=f"3 ({counters['item3']}/{15})", callback_data='item3')
-    button_4 = telebot.types.InlineKeyboardButton(text=f"4 ({counters['item4']}/{15})", callback_data='item4')
-    button_5 = telebot.types.InlineKeyboardButton(text=f"5 ({counters['item5']}/{15})", callback_data='item5')
-    button_6 = telebot.types.InlineKeyboardButton(text=f"6 ({counters['item6']}/{15})", callback_data='item6')
-    button_7 = telebot.types.InlineKeyboardButton(text=f"7 ({counters['item7']}/{15})", callback_data='item7')
-    button_8 = telebot.types.InlineKeyboardButton(text=f"8 ({counters['item8']}/{15})", callback_data='item8')
-    button_9 = telebot.types.InlineKeyboardButton(text=f"9 ({counters['item9']}/{15})", callback_data='item9')
-    button_10 = telebot.types.InlineKeyboardButton(text=f"10 ({counters['item10']}/{15})", callback_data='item10')
-    button_11 = telebot.types.InlineKeyboardButton(text=f"11 ({counters['item11']}/{15})", callback_data='item11')
-    button_12 = telebot.types.InlineKeyboardButton(text=f"12 ({counters['item12']}/{15})", callback_data='item12')
-    markup.row(button_1, button_2, button_3, button_4)
-    markup.row(button_5, button_6, button_7, button_8)
-    markup.row(button_9, button_10, button_11, button_12)
+    markup.row(telebot.types.InlineKeyboardButton(text="Обновить", callback_data="update"))
+
     return markup
-
-@bot.message_handler(commands=['btn_choice'])
-def send_keyboard(message):
-    markup = types.ReplyKeyboardMarkup()
-    itembtn1 = types.KeyboardButton('1')
-    itembtn2 = types.KeyboardButton('2')
-    itembtn3 = types.KeyboardButton('3')
-    itembtn4 = types.KeyboardButton('4')
-    itembtn5 = types.KeyboardButton('5')
-    itembtn6 = types.KeyboardButton('6')
-    itembtn7 = types.KeyboardButton('7')
-    itembtn8 = types.KeyboardButton('8')
-    itembtn9 = types.KeyboardButton('9')
-    itembtn10 = types.KeyboardButton('10')
-    itembtn11 = types.KeyboardButton('11')
-    itembtn12 = types.KeyboardButton('12')
-    markup.row(itembtn1, itembtn2,itembtn3,itembtn4)
-    markup.row(itembtn5, itembtn6,itembtn7,itembtn8)
-    markup.row(itembtn9, itembtn10,itembtn11,itembtn12)
-    bot.send_message(message.chat.id, "Choose one number:", reply_markup=markup)
-
+#bingo
 @bot.message_handler(commands=['btn_msg'])
 def welcome(message):
     with open('bingo.jpg', 'rb') as photo_file:
         bot.send_photo(message.chat.id, photo_file, caption='TestCaption',reply_markup=create_keyboard())
-
+#bingo
 @bot.callback_query_handler(func=lambda call: call.data[0:4] == 'item')
 def callback_inline(call):
     item_id = call.data
-    counters[item_id] = counters.get(item_id, 0) + 1
+    user_id = call.from_user.id
+
+    if user_id not in counters[item_id][1]:
+        counters[item_id][1].append(user_id)
+        counters[item_id][0]+=1
 
     new_markup = create_keyboard()
+    try:
+        bot.edit_message_reply_markup(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            reply_markup=new_markup
+        )
+    except telebot.apihelper.ApiTelegramException as e:
+        # Если прилетело "сообщение не изменено", просто игнорим
+        if "message is not modified" not in e.description:
+            raise
+    bot.answer_callback_query(call.id)
+#bingo
+@bot.callback_query_handler(func=lambda call: call.data == 'update')
+def callback_inline_update(call):
+    item_id = call.data
+    user_id = call.from_user.id
 
-    bot.edit_message_reply_markup(
-        chat_id=call.message.chat.id,
-        message_id=call.message.message_id,
-        reply_markup=new_markup
-    )
-    #bot.send_message(call.message.chat.id, 'Выбран '+ call.data[4:])
+    new_markup = create_keyboard()
+    try:
+        bot.edit_message_reply_markup(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            reply_markup=new_markup
+        )
+    except telebot.apihelper.ApiTelegramException as e:
+        # Если прилетело "сообщение не изменено", просто игнорим
+        if "message is not modified" not in e.description:
+            raise
     bot.answer_callback_query(call.id)
 
 bot.infinity_polling()
