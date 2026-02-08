c371b52ba5c11beb9db39fbe928129334e0190dd
Mustard <mustard.mar@gmail.com>
2026-02-08 17:47:24 +0300

    Добавлена кнопка для перехода в меню админа и выхода из него, немного причёсан код

--- Example and tests/test_telebot.py
+++ Example and tests/test_telebot.py
@@ -8,6 +8,12 @@ import telebot_db
 load_dotenv()
 token = os.getenv("BOT_TOKEN")
 bot=telebot.TeleBot(token)
+#admin_id = int(os.getenv("ADMIN_ID"))
+
+raw_ids = os.getenv("ADMIN_ID_LIST", "")
+# Превращаем строку в список целых чисел
+ids = [int(i) for i in raw_ids.split(",")] if raw_ids else []
+
 
 #Заглушка для бд
 counters={}
@@ -16,29 +22,37 @@ for i in range(1, 13):
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup()
-    btn_get_bingo = types.KeyboardButton('Получить бинго')
-    btn_join_bingo = types.KeyboardButton('Принять участие в бинго')
-    markup.add(btn_get_bingo)
-    markup.add(btn_join_bingo)
-    bot.send_message(message.chat.id, "Участвуйте в бинго!", reply_markup=markup)
 
-@bot.message_handler(func=lambda message: message.text in ["Получить бинго","Принять участие в бинго"])
+    #print(f"Пользователь {message.from_user.id} пишет сообщения")
+    bot.send_message(message.chat.id, "Участвуйте в бинго!", reply_markup=user_keyboard(message.from_user.id))
+
+@bot.message_handler(func=lambda message: message.text in ["Получить бинго","Принять участие в бинго","Войти в админку"])
 def handle_button(message):
     if message.text == "Получить бинго":
         welcome(message)
     if message.text == "Принять участие в бинго":
         print(f"Пользователь {message.from_user.id} принял участие в бинго")
+    if message.text == "Войти в админку" and message.from_user.id in ids:
+        bot.send_message(message.chat.id, "Да здравствует админка:", reply_markup=admin_keyboard())
 #for admin
-@bot.message_handler(commands=['button'])
-def send_keyboard(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Добавить пункт"))
-    keyboard.add(types.KeyboardButton("Узнать все пункты"))
-    keyboard.add(types.KeyboardButton("Удалить пункт"))
-    bot.send_message(message.chat.id, "Выберите кнопку:", reply_markup=keyboard)
+def admin_keyboard():
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("Добавить пункт"))
+    markup.add(types.KeyboardButton("Узнать все пункты"))
+    markup.add(types.KeyboardButton("Удалить пункт"))
+    markup.add(types.KeyboardButton("Выйти из админки"))
+    return markup
+def user_keyboard(user_id):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton('Получить бинго'))
+    markup.add(types.KeyboardButton('Принять участие в бинго'))
+
+    if user_id in ids:
+        btn_admin_button = types.KeyboardButton('Войти в админку')
+        markup.add(btn_admin_button)
+    return markup
 #for admin
-@bot.message_handler(func=lambda message: message.text in ["Добавить пункт","Узнать все пункты", "Удалить пункт"])
+@bot.message_handler(func=lambda message: message.text in ["Добавить пункт","Узнать все пункты", "Удалить пункт","Выйти из админки"])
 def handle_button(message):
     if message.text == "Добавить пункт":
         msg = bot.send_message(message.chat.id, "Введите название")
@@ -50,6 +64,9 @@ def handle_button(message):
         items = telebot_db.get_all_items()
         text_items = "\n".join([f"{i}: {text}" for i, text in items])
         bot.send_message(message.chat.id, text_items)
+    if message.text == "Выйти из админки":
+        bot.send_message(message.chat.id, "Возврат к бинго", reply_markup=user_keyboard(message.from_user.id))
+
 #for admin
 def add_item(message):
     telebot_db.add_new_item(message.text)
@@ -64,7 +81,7 @@ def delete_item(message):
     except ValueError:
         bot.send_message(message.chat.id, 'Ошибка!')
 #bingo_button
-def create_keyboard():
+def inline_keyboard():
     markup = types.InlineKeyboardMarkup()
     maxamount = 3
     text_complete = "✅"
@@ -85,18 +102,18 @@ def create_keyboard():
 @bot.message_handler(commands=['btn_msg'])
 def welcome(message):
     with open('bingo.jpg', 'rb') as photo_file:
-        bot.send_photo(message.chat.id, photo_file, caption='TestCaption',reply_markup=create_keyboard())
+        bot.send_photo(message.chat.id, photo_file, caption='TestCaption',reply_markup=inline_keyboard())
 #bingo
 @bot.callback_query_handler(func=lambda call: call.data[0:4] == 'item')
 def callback_inline(call):
     item_id = call.data
     user_id = call.from_user.id
-
-    if user_id not in counters[item_id][1]:
+    maxamount = 3
+    if (user_id not in counters[item_id][1]) and(counters[item_id][0]!=maxamount):
         counters[item_id][1].append(user_id)
         counters[item_id][0]+=1
 
-    new_markup = create_keyboard()
+    new_markup = inline_keyboard()
     try:
         bot.edit_message_reply_markup(
             chat_id=call.message.chat.id,
@@ -114,7 +131,7 @@ def callback_inline_update(call):
     item_id = call.data
     user_id = call.from_user.id
 
-    new_markup = create_keyboard()
+    new_markup = inline_keyboard()
     try:
         bot.edit_message_reply_markup(
             chat_id=call.message.chat.id,
